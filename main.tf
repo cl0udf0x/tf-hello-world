00a8d067632b2660e0546c9c1343225ff0a93312
Your Name <you@example.com>
2022-03-07 20:21:20 +0800

    Add greeting variable.

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+variable "greeting" {
+    description = "Greetings variable"
+    type = string
+    default = "dummy"
+}
+
 terraform {
   required_version = ">= 0.12.26"
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!"
+  value = var.greeting
 }
