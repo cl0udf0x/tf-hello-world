e9b221f624f25c172d3b25593c9cea6b0ee7936b
Your Name <you@example.com>
2022-04-29 08:09:13 +0800

    iam.

--- main.tf
+++ main.tf
@@ -6,3 +6,26 @@ terraform {
 output "hello_world" {
   value = var.greeting
 }
+
+module "role_ecs_task_exec" {
+  source              = "github.com/cloudposse/terraform-aws-iam-role.git?ref=0.16.2"
+  enabled             = true
+  context             = module.sw-services-iam-label
+  role_description    = module.sw-services-iam-label.id
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+}
+
+module "role_ecs_task" {
+  source           = "github.com/cloudposse/terraform-aws-iam-role.git?ref=0.16.2"
+  enabled          = true
+  context          = module.sw-services-iam-label
+  role_description = module.sw-services-iam-label.id
+  policy_documents = [
+    "${data.aws_iam_policy_document.sw_services_data.json}",
+    "${data.aws_iam_policy_document.sw_services_sqs_s3_data.json}",
+    "${data.aws_iam_policy_document.sw_services_reporting_data.json}"
+  ]
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+  ]
+}
